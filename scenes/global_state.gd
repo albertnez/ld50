962a8d0b64a16a6a936a3bc7fdabaaf14a980591
Albert M <albertmg7@gmail.com>
2022-04-03 15:14:28 +0200

    clearing a bit game state

--- scenes/global_state.gd
+++ scenes/global_state.gd
@@ -2,6 +2,7 @@ extends Node
 
 var level_completed = false
 var level_lost = false
+var in_menu = true
 
 func _ready() -> void:
 	pass
--- scenes/world.gd
+++ scenes/world.gd
@@ -1,14 +1,7 @@
 extends Node2D
 
 
-enum GameState {
-	MENU,
-	PLAYING,
-	GAME_OVER,
-}
-
 var _current_level := 0
-var _game_state: int
 var _toggle_in_menu_used := false
 onready var _player := $ScaledView/Player
 onready var _tilemap := $ScaledView/TileMap
@@ -21,7 +14,6 @@ func _ready() -> void:
 	EventBus.connect("person_crashed", self, "_on_EventBus_person_crashed")
 	EventBus.connect("level_restart", self, "_on_EventBus_level_restart")
 	EventBus.connect("level_completed", self, "_on_EventBus_level_completed")
-	_game_state = GameState.MENU
 	
 	EventBus.emit_signal("level_restart")
 	pass
@@ -31,13 +23,10 @@ func _start_level() -> void:
 	GlobalState.level_completed = false
 	GlobalState.level_lost = false
 	_toggle_in_menu_used = false
-	if _current_level == 0:
-		_game_state = GameState.MENU
-	else:
-		_game_state = GameState.PLAYING
+
 	var level = str(_current_level).pad_zeros(2)
 	var path = str("res://scenes/levels/level", level, ".tscn")
-	if _game_state == GameState.MENU:
+	if GlobalState.in_menu:
 		path = "res://scenes/levels/menu_level.tscn"
 	var tilemap_scene := load(path)
 	var new_tilemap = tilemap_scene.instance()
@@ -49,7 +38,7 @@ func _start_level() -> void:
 	_player.position = _tilemap.get_player_starting_world_position()
 	_trolley.set_process(false)
 	_trolley.position = Vector2.INF
-	if _game_state != GameState.MENU:
+	if not GlobalState.in_menu:
 		# On Menu, trolley starts upon player action
 		_trolley_timer.start(_tilemap.TROLLEY_WAIT_TIME)
 
@@ -58,33 +47,29 @@ func _start_level() -> void:
 func _process(delta: float) -> void:
 	
 	var player_toggled = false
-	match _game_state:
-		GameState.PLAYING, GameState.MENU:
-			var player_can_toggle = _tilemap.is_world_pos_a_toggable_tile(_player.position)
+	
+	var player_can_toggle = _tilemap.is_world_pos_a_toggable_tile(_player.position)
 #			if _game_state == GameState.MENU and _toggle_in_menu_used:
 #				player_can_toggle = false
-			_player.set_toggle_is_visible(player_can_toggle)
-			if Input.is_action_just_pressed("ui_accept") and player_can_toggle:
-				_tilemap.toggle_world_pos_cell(_player.position)
-				player_toggled = true
-			continue
-		GameState.MENU:
-			if player_toggled and not _toggle_in_menu_used:
-				EventBus.emit_signal("trolley_created")
-				_toggle_in_menu_used = true
-				
-		GameState.GAME_OVER:
-			if Input.is_action_just_pressed("restart"):
-				EventBus.emit_signal("level_restart")
-			return
+	_player.set_toggle_is_visible(player_can_toggle)
+	if Input.is_action_just_pressed("ui_accept") and player_can_toggle:
+		_tilemap.toggle_world_pos_cell(_player.position)
+		player_toggled = true
+
+	if GlobalState.in_menu:
+		if player_toggled and not _toggle_in_menu_used:
+			EventBus.emit_signal("trolley_created")
+			_toggle_in_menu_used = true
+	
+	if GlobalState.level_lost:
+		if Input.is_action_just_pressed("restart"):
+			EventBus.emit_signal("level_restart")
+
 
 
 func _on_LevelStartTrolleyTimer_timeout() -> void:
 	EventBus.emit_signal("trolley_created")
 
-
-func _on_EventBus_person_crashed() -> void:
-	_game_state = GameState.GAME_OVER
 
 
 func _on_EventBus_trolley_created() -> void:
@@ -100,4 +85,5 @@ func _on_EventBus_level_completed() -> void:
 	_level_completed_timer.start()
 	yield(_level_completed_timer, "timeout")
 	_current_level += 1
+	GlobalState.in_menu = false
 	EventBus.emit_signal("level_restart")
